985488ba07a7ff205d0478525e9a06c132b9fa4b
devbelal <150785553+devbelal@users.noreply.github.com>
2025-12-07 17:34:16 +0200

    feat: Add ride accept/cancel UI and trip logic

--- lib/features/map/widgets/ride_cancelation_list.dart
+++ lib/features/map/widgets/ride_cancelation_list.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:ride_sharing_user_app/common_widgets/custom_radio_button.dart';
@@ -17,35 +16,58 @@ class RideCancellationList extends StatefulWidget {
 class _RideCancellationListState extends State<RideCancellationList> {
   @override
   Widget build(BuildContext context) {
-    return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-        Text('why_do_you_want_to_cancel'.tr,style: textSemiBold.copyWith(fontSize: Dimensions.fontSizeLarge),),
-
-        const SizedBox(height: Dimensions.paddingSizeDefault,),
-      GetBuilder<TripController>(builder: (tripController){
-        int length = widget.isOngoing ? tripController.rideCancellationCauseList!.data!.ongoingRide!.length : tripController.rideCancellationCauseList!.data!.acceptedRide!.length;
-        return Container(decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(Dimensions.radiusDefault),
-            border: Border.all(color: Theme.of(context).primaryColor.withOpacity(0.15))),
-
-          child: ListView.separated(shrinkWrap: true,
-            itemCount: length,
-
-            physics: const NeverScrollableScrollPhysics(),padding: EdgeInsets.zero,
-            itemBuilder: (context,index){
-              return CustomRadioButton(text: widget.isOngoing ? tripController.rideCancellationCauseList!.data!.ongoingRide![index] :
-              tripController.rideCancellationCauseList!.data!.acceptedRide![index], isSelected: tripController.rideCancellationCurrentIndex == index,
-                  onTap: (){
-
-                tripController.setCancellationCurrentIndex(index);
-                setState(() {});},
-                length: length,index: index,);
-            },
-            separatorBuilder: (context,index){
-              return Divider(color: Theme.of(context).primaryColor.withOpacity(0.15),height: 0,);
-            },
-          ),
-        );
-      }),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'why_do_you_want_to_cancel'.tr,
+          style: textSemiBold.copyWith(fontSize: Dimensions.fontSizeLarge),
+        ),
+        const SizedBox(
+          height: Dimensions.paddingSizeDefault,
+        ),
+        GetBuilder<TripController>(builder: (tripController) {
+          int length = widget.isOngoing
+              ? tripController
+                  .rideCancellationCauseList!.data!.ongoingRide!.length
+              : tripController
+                  .rideCancellationCauseList!.data!.acceptedRide!.length;
+          return Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(Dimensions.radiusDefault),
+                border: Border.all(
+                    color: Theme.of(context).primaryColor.withOpacity(0.15))),
+            child: ListView.separated(
+              shrinkWrap: true,
+              itemCount: length,
+              physics: const NeverScrollableScrollPhysics(),
+              padding: EdgeInsets.zero,
+              itemBuilder: (context, index) {
+                return CustomRadioButton(
+                  text: widget.isOngoing
+                      ? tripController
+                          .rideCancellationCauseList!.data!.ongoingRide![index]
+                      : tripController.rideCancellationCauseList!.data!
+                          .acceptedRide![index],
+                  isSelected:
+                      tripController.rideCancellationCurrentIndex == index,
+                  onTap: () {
+                    tripController.setCancellationCurrentIndex(index);
+                    setState(() {});
+                  },
+                  length: length,
+                  index: index,
+                );
+              },
+              separatorBuilder: (context, index) {
+                return Divider(
+                  color: Theme.of(context).primaryColor.withOpacity(0.15),
+                  height: 0,
+                );
+              },
+            ),
+          );
+        }),
       ],
     );
   }
